0821abe3ad2b2532538e08bdc84a0d5a151113da
sang14 <sangya.14@gmail.com>
2016-09-28 00:09:21 +0530

    genric commit

--- chatbot101/views.py
+++ chatbot101/views.py
@@ -21,7 +21,7 @@ def index(request):
 def post_facebook_message(fbid,message_text):
 	post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
 	ouput_text=find(message_text)
-	response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":message_text}})
+	response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":output_text}})
 	status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
 	print status.json()
 
